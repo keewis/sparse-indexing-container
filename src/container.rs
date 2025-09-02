4c097e6ed9be0ba02bb43563125f2c410a7a2aaf
Justus Magin <keewis@posteo.de>
2025-09-02 15:15:12 +0200

    define the container trait

--- src/container.rs
+++ src/container.rs
@@ -1,11 +1,23 @@
-use ndarray::{SliceArg, Array1};
-
+use crate::errors::IndexError;
+use ndarray::{Array1, IxDyn, SliceArg};
 
 trait SparseContainer<T> {
-    fn data(&self) -> Array1<T>;
-    fn coords(&self) -> Vec<Array1<uint64>>;
+    fn fill_value(&self) -> T;
+    fn shape(&self) -> Vec<u64>;
 
-    fn oindex(&self, indexers: SliceArg) -> Self;
+    fn data(&self) -> &Array1<T>;
+    fn coords(&self) -> Vec<Array1<u64>>;
 
-    fn concat(parts: Vec<Container<T>>) -> Container<T>;
+    // orthogonal indexing
+    fn oindex<I>(&self, indexers: I) -> Result<Self, IndexError>
+    where
+        I: SliceArg<IxDyn>,
+        Self: Sized;
+
+    // fn concat(parts: Vec<Container<T>>, axis: u8) -> Result<Container<T>, ConcatError>;
+
+    // number of dimensions
+    fn ndim(&self) -> usize {
+        self.shape().len()
+    }
 }
