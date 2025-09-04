bc208c745c17e8e0a2fc5d34a271f02e541f6845
Justus Magin <keewis@posteo.de>
2025-09-04 18:01:03 +0200

    remove dummy pyo3 code

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,3 @@
-use pyo3::prelude::*;
-
 mod container;
 mod coo;
 mod errors;
-
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
-/// A Python module implemented in Rust.
-#[pymodule]
-fn sparse_indexing_container(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    Ok(())
-}
