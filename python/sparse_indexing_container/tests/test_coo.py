7f1b869d4fc16a023d0d05344a39e09beb903dc6
Justus Magin <keewis@posteo.de>
2025-09-08 20:58:04 +0200

    better checks of the container init

--- python/sparse_indexing_container/tests/test_coo.py
+++ python/sparse_indexing_container/tests/test_coo.py
@@ -11,13 +11,13 @@ def test_init():
 
     actual = COO(data=data, coords=coords, shape=shape, fill_value=fill_value)
 
-    (d, c, s, f) = actual.decompose()
-
-    np.testing.assert_equal(d, data)
+    np.testing.assert_equal(actual.data, data)
+    c = actual.coords
     np.testing.assert_equal(c[0], coords[0])
     np.testing.assert_equal(c[1], coords[1])
 
-    assert s == shape
-    assert f == fill_value
+    assert actual.shape == shape
+    assert actual.fill_value == fill_value
+    assert actual.nsv == data.size
 
     print(actual)
